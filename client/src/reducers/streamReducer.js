818f29265f93e6de03b49888e45c48fc8148377d
tugaykocaoglusst <tugay.kocaoglu@ssttekacademy.com>
2022-08-04 13:47:36 +0300

    Deleting Properties with Omit, Merging Lists of Records, Fetching a List of All Streams, Rendering All Streams, Associating Streams with Users

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,5 +1,4 @@
-import { act } from 'react-dom/test-utils';
-import { actionTypes } from 'redux-form';
+import _ from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -9,13 +8,17 @@ import {
 } from '../actions/types';
 
 export default ( state = {}, action ) => {
-    switch (actionTypes.type) {
+    switch (action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id')};
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload};
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload};
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload};
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
         default: 
             return state;
     }
